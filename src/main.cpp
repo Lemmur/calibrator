4a2643781fb2e5cc177ca749665ceb49c71f6d61
Lemmur <tema.ewseew@yandex.ru>
2020-01-18 22:44:20 +0300

    Calibrate not works

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
+#include <Adafruit_PWMServoDriver.h>
+
+#define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
+#define SERVO_FREQ 60 // Analog servos run at ~60 Hz updates
+
+Adafruit_PWMServoDriver board = Adafruit_PWMServoDriver(0x41);
+
+int getPulseFromDeg (int deg) {
+  return map(deg, 0, 180, SERVOMIN, SERVOMAX);
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  board.begin();
+  board.setOscillatorFrequency(27000000);
+  board.setPWMFreq(SERVO_FREQ);
+
+  board.setPWM(15, 0, getPulseFromDeg(0));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(1000);
+  board.setPWM(15, 0, getPulseFromDeg(180));
+  delay(1000);
+  board.setPWM(15, 0, getPulseFromDeg(0));
 }

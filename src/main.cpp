8c75320cdc741f29b398a0ab4cdb4ee947a1f662
Lemmur <tema.ewseew@yandex.ru>
2020-01-18 23:21:55 +0300

    Works

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,26 @@
 #include <Arduino.h>
 #include <Adafruit_PWMServoDriver.h>
 
-#define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
+#define SERVOMIN  100 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  580 // This is the 'maximum' pulse length count (out of 4096)
 #define SERVO_FREQ 60 // Analog servos run at ~60 Hz updates
 
-Adafruit_PWMServoDriver board = Adafruit_PWMServoDriver(0x41);
+Adafruit_PWMServoDriver board = Adafruit_PWMServoDriver(0x40);
 
 int getPulseFromDeg (int deg) {
   return map(deg, 0, 180, SERVOMIN, SERVOMAX);
 }
 
 void setup() {
+
   board.begin();
   board.setOscillatorFrequency(27000000);
-  board.setPWMFreq(SERVO_FREQ);
-
-  board.setPWM(15, 0, getPulseFromDeg(0));
+  board.setPWMFreq(60);
 }
 
 void loop() {
+  board.setPWM(0, 0, SERVOMIN );
   delay(1000);
-  board.setPWM(15, 0, getPulseFromDeg(180));
+  board.setPWM(0, 0, SERVOMAX );
   delay(1000);
-  board.setPWM(15, 0, getPulseFromDeg(0));
 }
